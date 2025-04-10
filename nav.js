73058fa2fd8967c82280376447fda6eacc41c563
adpgithub <anildabralpro@gmail.com>
2025-04-10 17:34:41 +0530

    copyright added

--- nav.js
+++ nav.js
@@ -1,3 +1,9 @@
+/**
+  * This file is protected by copyright (c) 2023-2025 by BlueSky Statistics, LLC.
+  * All rights reserved. The copy, modification, or distribution of this file is not
+  * allowed without the prior written permission from BlueSky Statistics, LLC.
+ */
+
 let t = getT('menutoolbar')
 const nav = () => ({
     "name": t('variables_top_level_title'),// {ns: 'menutoolbar'}),
